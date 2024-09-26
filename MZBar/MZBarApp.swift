321ef0710d019e4bc08f3cc0cbd2187c723414e6
Francois Denis <francois.denis@gmail.com>
2024-09-26 22:59:50 +0200

    Initial Commit

---
+++ MZBar/MZBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MZBarApp.swift
+//  MZBar
+//
+//  Created by Francois on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MZBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
